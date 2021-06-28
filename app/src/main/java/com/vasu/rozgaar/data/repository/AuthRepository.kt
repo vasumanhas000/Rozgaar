b84aa1e4bafa280b5307eddb5915194e8dcb7996
vasumanhas000 <vasumanhas000@gmail.com>
2021-06-28 11:59:26 +0530

    Initial Commits

---
+++ app/src/main/java/com/vasu/rozgaar/data/repository/AuthRepository.kt
@@ -0,0 +1,32 @@
+package com.vasu.rozgaar.data.repository
+
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+
+class AuthRepository {
+    private val auth = FirebaseAuth.getInstance();
+    private val _response = MutableLiveData<Boolean>();
+    private val _userResponse = MutableLiveData<Boolean>();
+    suspend fun signIn(credential : PhoneAuthCredential){
+       auth.signInWithCredential(credential).addOnCompleteListener(){task ->
+           if(task.isSuccessful){
+               Log.i("auth","success");
+               val user = task.result?.user
+               _response.postValue(true);
+           }else{
+               Log.i("auth","error");
+               if(task.exception is FirebaseAuthInvalidCredentialsException){
+//                   Wrong code
+                   _response.postValue(false)
+               }
+
+           }
+       }
+    }
+
+}
---
+++ app/src/main/java/com/vasu/rozgaar/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.vasu.rozgaar.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vasu.rozgaar.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
