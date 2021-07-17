dd3e832bc0c1e170ed3e686515a775990773bdf1
vasumanhas000 <vasumanhas000@gmail.com>
2021-07-17 22:07:16 +0530

    fixed navbar

--- app/src/main/java/com/vasu/rozgaar/ui/main/HomeActivity.kt
+++ app/src/main/java/com/vasu/rozgaar/ui/main/HomeActivity.kt
@@ -1,13 +1,22 @@
 package com.vasu.rozgaar.ui.main
 
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vasu.rozgaar.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        var bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navHostFragment : NavHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_home_activity) as NavHostFragment
+        var navController : NavController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
 }
